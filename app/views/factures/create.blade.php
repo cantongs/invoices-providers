9cfead80b2b6a9e594c5dfbc284c3b09adb96daf
goto <goto@constantinopla.(none)>
2014-02-17 22:37:45 +0100

    factures list screen 170214

---
+++ app/views/factures/create.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.layout')
+
+@section('content')
+    <div class="page-header">
+        <h1>Crea nova factura</h1>
+    </div>
+
+    <form class="form-horizontal form-small" action="{{ action('FacturesController@handleCreate') }}" method="post" role="form">
+        <div class="form-group">
+            <div class="col-lg-2">
+                <label for="idfactura">Idfactura</label>
+                <input type="text" class="form-control" name="idfactura" placeholder="10/2013" />
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-lg-2">
+                <label for="proveidor">Proveïdor</label>
+                <select name="idproveidor">
+                    <?php $proveidors = Proveidor::all(); ?>
+                    @foreach($proveidors as $proveidor)
+                    <option value="{{ $proveidor->id }}">{{ $proveidor->nom }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+         <div class="form-group">
+            <div class="col-lg-2">
+                <label for="data">Data</label>
+                <input type="text" class="form-control" name="data" placeholder="17-02-2014" />
+            </div>
+        </div>
+        <br>
+        
+        <input type="submit" value="Create" class="btn btn-primary" />
+        <a href="{{ action('FacturesController@index') }}" class="btn btn-link">Cancel</a>
+    </form>
+@stop
